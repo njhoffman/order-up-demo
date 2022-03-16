49dd827e958a7236a69783124af717918a244247
Nicholas Hoffman <njhoffman1982@gmail.com>
2022-03-15 23:44:06 -0500

    Add developer dependencies to tools folder

---
+++ tools/tools.go
@@ -0,0 +1,9 @@
+//nolint:golint,unused
+package tools
+
+// devtools included here for easy installation
+import (
+	"github.com/go-task/task/v3/cmd/task"
+	"github.com/golangci/golangci-lint/cmd/golangci-lint"
+	"github.com/kyoh86/richgo"
+)
